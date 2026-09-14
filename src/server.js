66374ae95dee4820af8ecffb3af7aa6af57a11cf
Miranda Casan <miranda.casan_ext@nunsys.com>
2026-09-13 20:51:53 -0400

    fix path server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "./src/views");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static("src/public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 
@@ -71,3 +72,5 @@ app.get("/db-test", async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
